2d0835fbfc0f08356ce598e2f150841efb4fd04d
Nafees <abdur17@student.sust.edu>
2022-05-08 02:21:58 +0600

    api created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,14 +8,65 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
-    res.send('hello')
-});
 
-app.get('/products', (req, res) => {
-    res.send([{"id": 1, "name": "nafees"}, {"id": 2, "name": "nakees"}, {"id": 3, "name": "nabees"}])
+const uri = "mongodb+srv://dbserver1:EkSsh05KlZzFG547@cluster0.4l5l2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        await client.connect();
+        const itemCollection = client.db("wirehouse").collection("products");
+
+        app.get('/products', async(req, res) =>{
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+        app.get('/products/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await itemCollection.findOne(query);
+            res.send(result);
+        });
+
+        // POST items
+        app.post('/products', async(req, res) =>{
+            const newItem = req.body;
+            console.log('adding new item', newItem);
+            const result = await itemCollection.insertOne(newItem);
+            res.send(result)
+        });
+
+        // update user
+        
+
+        // app.post('/productss', (req, res) =>  {
+        //     const newItems = req.body;
+        //     console.log('Adding', newItems);
+        //     res.send({result: 'Items Added successfully!!'})
+        // });
+        // const products =  {name: "suzuki" , model: "sf"};
+        // const result = await userCollection.insertOne(products);
+        // console.log(`inserted: ${result.insertedId}`);
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Running...')
 })
 
 app.listen(port, () => {
     console.log('listening');
 })
+
+// user:dbserver1
+// pass:EkSsh05KlZzFG547
