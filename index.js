5e76a706ef87325c4ff058308af865eab991a6a6
Nafees <abdur17@student.sust.edu>
2022-05-09 18:07:55 +0600

    heroku setup done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+// heroku setup
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4l5l2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -50,8 +50,7 @@ async function run() {
             const options = { upsert: true };
             const updatedDoc = {
                 $set: {
-                    name: updatedItem.name,
-                    model: updatedItem.model
+                    quantity: updatedDoc.quantity
                 }
                 // "$inc": {
                 //     name: 10
